224f438bb03009c4c973657ea7cb69eb4be8d50e
MortezaZandi <94436010+MortezaZandi@users.noreply.github.com>
2024-06-21 02:11:58 +0330

    add wcfservice manager

--- SimpleWCFService_auto/WCFServer/Form1.cs
+++ SimpleWCFService_auto/WCFServer/Form1.cs
@@ -18,17 +18,14 @@ namespace WCFServer
 {
     public partial class Form1 : Form
     {
-        private readonly MZWCFService serviceClass;
-        private readonly Type serviceInterface;
-        private WebServiceHost host;
+        private readonly WCFServiceManager wcfServiceManager;
 
         public Form1()
         {
             InitializeComponent();
-
-            this.serviceClass = new MZWCFService();
-            this.serviceInterface = typeof(IMZWCFService);
             WCFExtra.LoggingOperationInvoker.OnNewLog += MZWCFService_OnRequest;
+            this.wcfServiceManager = new WCFServiceManager();
+            this.wcfServiceManager.RegisterService(Environment.MachineName, nameof(MZWCFService), 2050, typeof(MZWCFService), typeof(IMZWCFService));
         }
 
         private void MZWCFService_OnRequest(string message)
@@ -38,32 +35,9 @@ namespace WCFServer
 
         private void button1_Click(object sender, EventArgs e)
         {
-            try
+             try
             {
-                var baseAddress = new Uri("http://ks40:2050/MZWCFService/");
-
-                host = new WebServiceHost(serviceClass, baseAddress);
-
-                AddDescriptionBehavior(host);
-
-                AddServiceEndpoint(this.host, this.serviceInterface);
-
-                //ServiceMetadataBehavior metadataBehavior;
-                //metadataBehavior = host.Description.Behaviors.Find<ServiceMetadataBehavior>();
-                //if (metadataBehavior == null)
-                //{
-                //    metadataBehavior = new ServiceMetadataBehavior();
-                //    metadataBehavior.HttpGetEnabled = true;
-                //    host.Description.Behaviors.Add(metadataBehavior);
-                //}
-                //else
-                //{
-                //    metadataBehavior.HttpGetEnabled = true;
-                //}
-
-                host.Authorization.ServiceAuthorizationManager = new WCFServiceAuthorizationManager();
-
-                host.Open();
+                this.wcfServiceManager.StartAll();
 
                 listBox1.Items.Add("WCF Service has been started successfully.");
             }
@@ -72,36 +46,6 @@ namespace WCFServer
                 listBox1.Items.Add(ex.Message);
             }
         }
-
-        private static void AddServiceEndpoint(ServiceHost host, Type serviceInterfaceType)
-        {
-            ServiceEndpoint restEndpoint = host.AddServiceEndpoint(serviceInterfaceType, new WebHttpBinding() { MaxReceivedMessageSize = 1073741824 }, "");
-            WebHttpBehavior restWebHttpBehavior = new WebHttpBehavior();
-            restWebHttpBehavior.HelpEnabled = true;
-            restEndpoint.EndpointBehaviors.Add(restWebHttpBehavior);
-        }
-
-        private static void AddDescriptionBehavior(ServiceHost host)
-        {
-            host.Description.Behaviors.Add(new ServiceMetadataBehavior { HttpGetEnabled = true });
-
-            ServiceDebugBehavior debug = host.Description.Behaviors.Find<ServiceDebugBehavior>();
-            // if not found - add behavior with setting turned on
-            if (debug == null)
-            {
-                host.Description.Behaviors.Add(new ServiceDebugBehavior() { IncludeExceptionDetailInFaults = true });
-            }
-            else
-            {
-                // make sure setting is turned ON
-                if (!debug.IncludeExceptionDetailInFaults)
-                {
-                    debug.IncludeExceptionDetailInFaults = true;
-                }
-            }
-
-        }
-        
         
         private Process client;
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
---
+++ SimpleWCFService_auto/WCFServer/WCFServiceManager.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Description;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+using WCFServer.WCFExtra;
+
+namespace WCFServer
+{
+    public class WCFServiceManager
+    {
+        private readonly Dictionary<string, ServiceHost> registeredServices;
+
+        public WCFServiceManager()
+        {
+            registeredServices = new Dictionary<string, ServiceHost>();
+        }
+
+        public void RegisterService(string hostName, string serviceName, int portNumber, Type serviceClassTypem, Type servericeInterfaceType)
+        {
+            if (this.registeredServices.ContainsKey(hostName))
+            {
+                throw new InvalidOperationException($"Service '{serviceName}' already registered.");
+            }
+
+            var baseAddress = new Uri($"http://{hostName}:{portNumber}/{serviceName}/");
+
+            var host = new WebServiceHost(CreateServiceInstance(serviceClassTypem), baseAddress);
+
+            AddDescriptionBehavior(host);
+
+            AddServiceEndpoint(host, servericeInterfaceType);
+
+            //host.Authorization.ServiceAuthorizationManager = new WCFServiceAuthorizationManager();
+
+            this.registeredServices.Add(serviceName, host);
+        }
+
+        private object CreateServiceInstance(Type serviceType)
+        {
+            return Activator.CreateInstance(serviceType);
+        }
+
+        private static void AddServiceEndpoint(ServiceHost host, Type serviceInterfaceType)
+        {
+            ServiceEndpoint restEndpoint = host.AddServiceEndpoint(serviceInterfaceType, new WebHttpBinding() { MaxReceivedMessageSize = 1073741824 }, "");
+            WebHttpBehavior restWebHttpBehavior = new WebHttpBehavior();
+            restWebHttpBehavior.HelpEnabled = true;
+            restEndpoint.EndpointBehaviors.Add(restWebHttpBehavior);
+        }
+
+        private static void AddDescriptionBehavior(ServiceHost host)
+        {
+            host.Description.Behaviors.Add(new ServiceMetadataBehavior { HttpGetEnabled = true });
+
+            ServiceDebugBehavior debug = host.Description.Behaviors.Find<ServiceDebugBehavior>();
+            // if not found - add behavior with setting turned on
+            if (debug == null)
+            {
+                host.Description.Behaviors.Add(new ServiceDebugBehavior() { IncludeExceptionDetailInFaults = true });
+            }
+            else
+            {
+                // make sure setting is turned ON
+                if (!debug.IncludeExceptionDetailInFaults)
+                {
+                    debug.IncludeExceptionDetailInFaults = true;
+                }
+            }
+
+        }
+
+        public void StartAll()
+        {
+            foreach (string serviceName in this.registeredServices.Keys)
+            {
+                try
+                {
+                    this.registeredServices[serviceName].Open();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception($"Error in starting service '{serviceName}'", ex);
+                }
+            }
+        }
+
+        public void StopAll()
+        {
+            foreach (string serviceName in this.registeredServices.Keys)
+            {
+                this.registeredServices[serviceName].Close();
+            }
+        }
+    }
+}
