224e401d296c9696bb9cb8a996060f43507a7189
MortezaZandi <94436010+MortezaZandi@users.noreply.github.com>
2024-11-04 17:47:44 +0330

    Add Customized FontDialog

---
+++ CustomizeFontDialog/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace TestFontDialog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChangeFont = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblFontName = new System.Windows.Forms.Label();
+            this.lblFontFamily = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblFontSize = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblFontStyle = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblFontSample = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnChangeFont
+            // 
+            this.btnChangeFont.Location = new System.Drawing.Point(15, 198);
+            this.btnChangeFont.Name = "btnChangeFont";
+            this.btnChangeFont.Size = new System.Drawing.Size(75, 23);
+            this.btnChangeFont.TabIndex = 0;
+            this.btnChangeFont.Text = "Change Font";
+            this.btnChangeFont.UseVisualStyleBackColor = true;
+            this.btnChangeFont.Click += new System.EventHandler(this.btnChangeFont_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Font Name:";
+            // 
+            // lblFontName
+            // 
+            this.lblFontName.AutoSize = true;
+            this.lblFontName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFontName.Location = new System.Drawing.Point(91, 9);
+            this.lblFontName.Name = "lblFontName";
+            this.lblFontName.Size = new System.Drawing.Size(11, 13);
+            this.lblFontName.TabIndex = 2;
+            this.lblFontName.Text = " ";
+            this.lblFontName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblFontFamily
+            // 
+            this.lblFontFamily.AutoSize = true;
+            this.lblFontFamily.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFontFamily.Location = new System.Drawing.Point(91, 41);
+            this.lblFontFamily.Name = "lblFontFamily";
+            this.lblFontFamily.Size = new System.Drawing.Size(11, 13);
+            this.lblFontFamily.TabIndex = 4;
+            this.lblFontFamily.Text = " ";
+            this.lblFontFamily.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Font Family:";
+            // 
+            // lblFontSize
+            // 
+            this.lblFontSize.AutoSize = true;
+            this.lblFontSize.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFontSize.Location = new System.Drawing.Point(91, 75);
+            this.lblFontSize.Name = "lblFontSize";
+            this.lblFontSize.Size = new System.Drawing.Size(11, 13);
+            this.lblFontSize.TabIndex = 6;
+            this.lblFontSize.Text = " ";
+            this.lblFontSize.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 75);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Font Size:";
+            // 
+            // lblFontStyle
+            // 
+            this.lblFontStyle.AutoSize = true;
+            this.lblFontStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFontStyle.Location = new System.Drawing.Point(91, 113);
+            this.lblFontStyle.Name = "lblFontStyle";
+            this.lblFontStyle.Size = new System.Drawing.Size(11, 13);
+            this.lblFontStyle.TabIndex = 8;
+            this.lblFontStyle.Text = " ";
+            this.lblFontStyle.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 113);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(57, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Font Style:";
+            // 
+            // lblFontSample
+            // 
+            this.lblFontSample.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblFontSample.Location = new System.Drawing.Point(287, 21);
+            this.lblFontSample.Name = "lblFontSample";
+            this.lblFontSample.Size = new System.Drawing.Size(177, 161);
+            this.lblFontSample.TabIndex = 9;
+            this.lblFontSample.Text = "Sample Text\r\nمتن نمونه";
+            this.lblFontSample.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 233);
+            this.Controls.Add(this.lblFontSample);
+            this.Controls.Add(this.lblFontStyle);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblFontSize);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblFontFamily);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblFontName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnChangeFont);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChangeFont;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblFontName;
+        private System.Windows.Forms.Label lblFontFamily;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblFontSize;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblFontStyle;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblFontSample;
+    }
+}
+
---
+++ CustomizeFontDialog/Form1.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestFontDialog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChangeFont_Click(object sender, EventArgs e)
+        {
+            var fontDialog = new FontDialogEx();
+
+            fontDialog.EnableScripts = false;
+            fontDialog.ScriptControlTitle = "Disabled";
+            fontDialog.Font = CreateDefaultFont();
+
+            if (fontDialog.ShowDialog() == DialogResult.OK)
+            {
+                lblFontSample.Font = fontDialog.Font;
+
+                lblFontName.Text = fontDialog.Font.Name;
+                lblFontFamily.Text = fontDialog.Font.FontFamily.Name.ToString();
+                lblFontSize.Text = fontDialog.Font.Size.ToString();
+                lblFontStyle.Text = fontDialog.Font.Style.ToString();
+            }
+        }
+
+        private Font CreateDefaultFont()
+        {
+            return new Font("Tahoma", 10, FontStyle.Bold);
+        }
+    }
+
+    public class FontDialogEx : FontDialog
+    {
+        public FontDialogEx()
+        {
+            ShowScripts = true;
+            EnableScripts = true;
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern int GetWindowTextLength(IntPtr hWnd);
+
+        [return: MarshalAs(UnmanagedType.Bool)]
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool GetWindowInfo(IntPtr hwnd, ref WINDOWINFO pwi);
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern bool SetWindowText(IntPtr hwnd, String lpString);
+
+
+        public bool EnableScripts { get; set; }
+
+        public bool ShowScripts { get; set; }
+
+        public string ScriptControlTitle { get; set; }
+
+        protected override IntPtr OwnerWndProc(IntPtr hWnd, int msg, IntPtr wparam, IntPtr lparam)
+        {
+            var w3 = GetText(lparam);
+            if (w3 == "Font")
+            {
+                var aw = new WindowHandleInfo(lparam).GetAllChildHandles();
+                foreach (var h in aw)
+                {
+                    var wn = GetText(h);
+
+                    if (wn.Replace("&", string.Empty).StartsWith("Script"))
+                    {
+                        var nextWindow = aw[aw.IndexOf(h) + 1];
+
+                        WINDOWINFO info = new WINDOWINFO();
+                        info.cbSize = (uint)Marshal.SizeOf(info);
+                        GetWindowInfo(nextWindow, ref info);
+
+                        if (info.atomWindowType == 49180)
+                        {
+                            if (!EnableScripts)
+                            {
+                                EnableWindow(nextWindow, false);
+
+                                if (!string.IsNullOrEmpty(ScriptControlTitle))
+                                {
+                                    SetWindowText(h, ScriptControlTitle);
+                                }
+                            }
+
+                            if (!ShowScripts)
+                            {
+                                ShowWindow(nextWindow, 0);
+                                ShowWindow(h, 0);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return base.OwnerWndProc(hWnd, msg, wparam, lparam);
+        }
+
+        public static string GetText(IntPtr hWnd)
+        {
+            try
+            {
+                // Allocate correct string length first
+                int length = GetWindowTextLength(hWnd);
+                StringBuilder sb = new StringBuilder(length + 1);
+                GetWindowText(hWnd, sb, sb.Capacity);
+                return sb.ToString();
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct WINDOWINFO
+        {
+            public uint cbSize;
+            public RECT rcWindow;
+            public RECT rcClient;
+            public uint dwStyle;
+            public uint dwExStyle;
+            public uint dwWindowStatus;
+            public uint cxWindowBorders;
+            public uint cyWindowBorders;
+            public ushort atomWindowType;
+            public ushort wCreatorVersion;
+
+            public WINDOWINFO(Boolean? filler) : this()   // Allows automatic initialization of "cbSize" with "new WINDOWINFO(null/true/false)".
+            {
+                cbSize = (UInt32)(Marshal.SizeOf(typeof(WINDOWINFO)));
+            }
+
+        }
+
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left, Top, Right, Bottom;
+
+            public RECT(int left, int top, int right, int bottom)
+            {
+                Left = left;
+                Top = top;
+                Right = right;
+                Bottom = bottom;
+            }
+
+            public RECT(System.Drawing.Rectangle r) : this(r.Left, r.Top, r.Right, r.Bottom) { }
+
+            public int X
+            {
+                get { return Left; }
+                set { Right -= (Left - value); Left = value; }
+            }
+
+            public int Y
+            {
+                get { return Top; }
+                set { Bottom -= (Top - value); Top = value; }
+            }
+
+            public int Height
+            {
+                get { return Bottom - Top; }
+                set { Bottom = value + Top; }
+            }
+
+            public int Width
+            {
+                get { return Right - Left; }
+                set { Right = value + Left; }
+            }
+
+            public System.Drawing.Point Location
+            {
+                get { return new System.Drawing.Point(Left, Top); }
+                set { X = value.X; Y = value.Y; }
+            }
+
+            public System.Drawing.Size Size
+            {
+                get { return new System.Drawing.Size(Width, Height); }
+                set { Width = value.Width; Height = value.Height; }
+            }
+
+            public static implicit operator System.Drawing.Rectangle(RECT r)
+            {
+                return new System.Drawing.Rectangle(r.Left, r.Top, r.Width, r.Height);
+            }
+
+            public static implicit operator RECT(System.Drawing.Rectangle r)
+            {
+                return new RECT(r);
+            }
+
+            public static bool operator ==(RECT r1, RECT r2)
+            {
+                return r1.Equals(r2);
+            }
+
+            public static bool operator !=(RECT r1, RECT r2)
+            {
+                return !r1.Equals(r2);
+            }
+
+            public bool Equals(RECT r)
+            {
+                return r.Left == Left && r.Top == Top && r.Right == Right && r.Bottom == Bottom;
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (obj is RECT)
+                    return Equals((RECT)obj);
+                else if (obj is System.Drawing.Rectangle)
+                    return Equals(new RECT((System.Drawing.Rectangle)obj));
+                return false;
+            }
+
+            public override int GetHashCode()
+            {
+                return ((System.Drawing.Rectangle)this).GetHashCode();
+            }
+
+            public override string ToString()
+            {
+                return string.Format(System.Globalization.CultureInfo.CurrentCulture, "{{Left={0},Top={1},Right={2},Bottom={3}}}", Left, Top, Right, Bottom);
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        public struct WNDCLASSEX
+        {
+            [MarshalAs(UnmanagedType.U4)]
+            public uint cbSize;
+            [MarshalAs(UnmanagedType.U4)]
+            public int style;
+            public IntPtr lpfnWndProc; // not WndProc
+            public int cbClsExtra;
+            public int cbWndExtra;
+            public IntPtr hInstance;
+            public IntPtr hIcon;
+            public IntPtr hCursor;
+            public IntPtr hbrBackground;
+            public string lpszMenuName;
+            public string lpszClassName;
+            public IntPtr hIconSm;
+
+            //Use this function to make a new one with cbSize already filled in.
+            //For example:
+            //var WndClss = WNDCLASSEX.Build()
+            public static WNDCLASSEX Build()
+            {
+                var nw = new WNDCLASSEX();
+                nw.cbSize = (uint)Marshal.SizeOf(typeof(WNDCLASSEX));
+                return nw;
+            }
+        }
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool EnableWindow(IntPtr hWnd, bool bEnable);
+        public class WindowHandleInfo
+        {
+            private delegate bool EnumWindowProc(IntPtr hwnd, IntPtr lParam);
+
+            [DllImport("user32")]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            private static extern bool EnumChildWindows(IntPtr window, EnumWindowProc callback, IntPtr lParam);
+
+            private IntPtr _MainHandle;
+
+            public WindowHandleInfo(IntPtr handle)
+            {
+                this._MainHandle = handle;
+            }
+
+            public List<IntPtr> GetAllChildHandles()
+            {
+                List<IntPtr> childHandles = new List<IntPtr>();
+
+                GCHandle gcChildhandlesList = GCHandle.Alloc(childHandles);
+                IntPtr pointerChildHandlesList = GCHandle.ToIntPtr(gcChildhandlesList);
+
+                try
+                {
+                    EnumWindowProc childProc = new EnumWindowProc(EnumWindow);
+                    EnumChildWindows(this._MainHandle, childProc, pointerChildHandlesList);
+                }
+                finally
+                {
+                    gcChildhandlesList.Free();
+                }
+
+                return childHandles;
+            }
+
+            private bool EnumWindow(IntPtr hWnd, IntPtr lParam)
+            {
+                GCHandle gcChildhandlesList = GCHandle.FromIntPtr(lParam);
+
+                if (gcChildhandlesList == null || gcChildhandlesList.Target == null)
+                {
+                    return false;
+                }
+
+                List<IntPtr> childHandles = gcChildhandlesList.Target as List<IntPtr>;
+                childHandles.Add(hWnd);
+
+                return true;
+            }
+        }
+    }
+
+}
